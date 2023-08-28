2e204b4bd52d732b04441277d3e918f9c6656a4d
bebeken <26348722+bebeken@users.noreply.github.com>
2023-08-29 00:15:03 +0900

    add formula

---
+++ homebrew-genuuids.rb
@@ -0,0 +1,7 @@
+class HomebrewGenuuids < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/bebeken/homebrew-genuuids/archive/refs/tags/v1.0.tar.gz"
+  sha256 "420825331cc80655ef574ec53b7e5ec3893aee8fefc0825edaaa0a0bc04de101"
+  license ""
+end
