2a3a948ac98f8c2d17b20e6954466e54f3c42e6b
bebeken <26348722+bebeken@users.noreply.github.com>
2023-08-29 01:30:37 +0900

    fix formula

--- homebrew-genuuids.rb
+++ homebrew-genuuids.rb
@@ -1,7 +1,11 @@
 class HomebrewGenuuids < Formula
   desc ""
   homepage ""
-  url "https://github.com/bebeken/homebrew-genuuids/archive/refs/tags/v1.0.tar.gz"
+  url "https://github.com/bebeken/homebrew-genuuids/archive/v1.0.tar.gz"
   sha256 "420825331cc80655ef574ec53b7e5ec3893aee8fefc0825edaaa0a0bc04de101"
   license ""
+
+  def install
+    bin.install "genuuids"
+  end
 end
